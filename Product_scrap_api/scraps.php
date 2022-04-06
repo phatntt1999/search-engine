356c81932e3d5e851b6c36a0d9912486ad7f4e8f
PhatScofield <scofield.nttp@gmail.com>
2022-04-06 17:19:04 +0700

    Product scrapt api

---
+++ Product_scrap_api/scraps.php
@@ -0,0 +1,26 @@
+<?php
+/*Plugin Name: Scrapper PluginDescription:Plugin URI:Author:      Austin NguyenVersion:     1.0.0*/
+if (!defined('ABSPATH')) {
+    exit;
+}
+include(plugin_dir_path(__FILE__) . 'Dir/simple_html_dom.php');
+
+function scrap_add_toplevel_menu()
+{
+
+
+    add_menu_page(
+        'Scrap Settings',
+        'Product Scrap',
+        'manage_options',
+        'scrap',
+        'scrap_display_settings_page',
+        'dashicons-admin-generic',
+        null
+    );
+
+}
+
+add_action('admin_menu', 'scrap_add_toplevel_menu');
+
+include(plugin_dir_path(__FILE__) . 'Dir/settings.php');
